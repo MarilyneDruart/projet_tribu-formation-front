f38d1ea95e24d0000ceed02bf8b29d6bad171f5c
LaurentArcos <laurent.arcos@gmail.com>
2022-08-16 13:22:24 +0200

    MainImage Component ok, style ok mobile and widescreen

--- tribu/src/Components/App/App.js
+++ tribu/src/Components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+
 import Header from '../Header/Header';
+import MainImage from '../MainImage/MainImage';
+
 import '../../styles/styles.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainImage />
     </div>
   );
 }
---
+++ tribu/src/Components/MainImage/MainImage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../../styles/styles.scss';
+
+import img from '../../assets/images/Rome-Main.jpg';
+
+function MainImage() {
+  return (
+    <div className="MainImage">
+      <img src={img} className="MainImage-img" alt="Rome" />
+      <div className="MainImage-content">
+        <h2 className="MainImage-title">ROME</h2>
+      </div>
+    </div>
+  );
+}
+
+export default MainImage;
