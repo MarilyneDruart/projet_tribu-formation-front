ee5356a88573ea4d461cc45f1472153a12add347
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-17 22:32:32 +0200

    case OPEN_INSCRIPTION_FORM settled into inscriptionForm reducer

--- tribu/src/store/inscriptionForm.js
+++ tribu/src/store/inscriptionForm.js
@@ -1,11 +1,18 @@
 import { OPEN_INSCRIPTION_FORM } from '../actions/actions';
 
 const initialState = {
+  isDisplayed: false,
   isLogged: false,
 };
 
 function inscriptionForm(state = initialState, action = {}) {
   switch (action.type) {
+    case OPEN_INSCRIPTION_FORM:
+      return {
+        ...state,
+        isDisplayed: true,
+      };
+
     default:
       return state;
   }
