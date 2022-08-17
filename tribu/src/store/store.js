d86bb198bdf706a57002c731d08c5936f6898fbe
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-17 22:53:58 +0200

    updating store with composeEnhancers and applyMiddleware

--- tribu/src/store/store.js
+++ tribu/src/store/store.js
@@ -1,10 +1,15 @@
 import {
-  createStore, compose,
+  createStore, applyMiddleware, compose,
 } from 'redux';
 
 import inscriptionForm from './inscriptionForm';
 
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const myMiddleWares = composeEnhancers(
+  applyMiddleware(loginMiddleware),
+);
 
 const store = createStore(
   inscriptionForm, /* preloadedState, */
