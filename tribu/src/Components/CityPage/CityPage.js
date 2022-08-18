0696e0af05afd3ee715139dd9acc68fc55e02a07
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-18 17:06:50 +0200

    first axios request to get a city informations

--- tribu/src/Components/CityPage/CityPage.js
+++ tribu/src/Components/CityPage/CityPage.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import axios from 'axios';
 
 import '../../styles/styles.scss';
-import MainImage from '../MainImage/MainImage';
 
 function CityPage() {
+  const [city, setCity] = useState([]);
+
+  const fetchResults = () => {
+    axios
+      .get('http://svitlana-burlak-kuzoski.vpnuser.lan:8000/api/cities/31')
+      .then((response) => {
+        console.log(response.data);
+        setCity(response.data);
+        console.log(Object.keys(city));
+      })
+      .catch((error) => {
+        console.log('Erreur !', error);
+      });
+  };
+
+  useEffect(() => {
+    fetchResults();
+  }, []);
+
   return (
-    <MainImage />
+    <div className="CityPage">
+      <img src={city.image} className="CityPage_img" alt="ville" />
+      <div className="CityPage_content">
+        <h2 className="CityPage_title">{city.name}</h2>
+      </div>
+      <p className="CityPage_description">{city.description}</p>
+    </div>
+
   );
 }
 
