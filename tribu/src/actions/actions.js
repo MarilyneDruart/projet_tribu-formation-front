42fadceff48299a43feca7d897acbbcb54ab2431
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-18 22:18:15 +0200

    closeInscritionForm action created

--- tribu/src/actions/actions.js
+++ tribu/src/actions/actions.js
@@ -2,3 +2,8 @@ export const TOGGLE_INSCRIPTION_FORM = 'TOGGLE_INSCRIPTION_FORM';
 export const toggleinscriptionForm = () => ({
   type: TOGGLE_INSCRIPTION_FORM,
 });
+
+export const CLOSE_INSCRIPTION_FORM = 'CLOSE_INSCRIPTION_FORM';
+export const closeinscriptionForm = () => ({
+  type: CLOSE_INSCRIPTION_FORM,
+});
