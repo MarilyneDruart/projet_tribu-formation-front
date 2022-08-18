fec97e6065f379f73de3279641da7ad92221a653
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-18 22:19:38 +0200

    adding CLOSE_INSCRIPTION_FORM into inscriptionForm reducer

--- tribu/src/store/inscriptionForm.js
+++ tribu/src/store/inscriptionForm.js
@@ -1,4 +1,4 @@
-import { TOGGLE_INSCRIPTION_FORM } from '../actions/actions';
+import { CLOSE_INSCRIPTION_FORM, TOGGLE_INSCRIPTION_FORM } from '../actions/actions';
 
 const initialState = {
   isDisplayed: false,
@@ -7,10 +7,15 @@ const initialState = {
 function inscriptionForm(state = initialState, action = {}) {
   switch (action.type) {
     case TOGGLE_INSCRIPTION_FORM:
-      console.log('on passe dans l\'action');
       return {
         ...state,
         isDisplayed: !state.isDisplayed,
+      };
+
+    case CLOSE_INSCRIPTION_FORM:
+      return {
+        ...state,
+        isDisplayed: false,
       };
 
     default:
