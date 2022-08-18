ce3e333c59fb16e091b2036f27d9d793b9e2388c
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-18 23:20:03 +0200

    FETCH_CITIES action created

--- tribu/src/Components/Home/Home.js
+++ tribu/src/Components/Home/Home.js
@@ -4,7 +4,13 @@ import '../../styles/styles.scss';
 
 function Home() {
   return (
-    <MainImage />
+    <div className="Home">
+      <MainImage />
+      {/* On veut afficher les villes de notre BDD
+          On map sur notre tableau de villes
+          et on affiche chaque ville sous forme de card cliquable (liens)
+          on récupère les noms des villes et leur image avec une requête axios GET */}
+    </div>
   );
 }
 
---
+++ tribu/src/actions/cities.js
@@ -0,0 +1,4 @@
+export const FETCH_CITIES = 'FETCH_CITIES';
+export const fetchCities = () => ({
+  type: FETCH_CITIES,
+});
