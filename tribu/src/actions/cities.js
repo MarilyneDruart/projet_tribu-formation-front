05b168c8e9c75f0b6849575ed369ef2e8d8592f0
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-22 15:28:18 +0200

    adding FETCH_CITY action

--- tribu/src/actions/cities.js
+++ tribu/src/actions/cities.js
@@ -3,6 +3,11 @@ export const fetchCities = () => ({
   type: FETCH_CITIES,
 });
 
+export const FETCH_CITY = 'FETCH_CITY';
+export const fetchCity = () => ({
+  type: FETCH_CITY,
+});
+
 export const SET_CITIES_LIST = 'SET_CITIES_LIST';
 export const setCitiesList = (cities) => ({
   type: SET_CITIES_LIST,
