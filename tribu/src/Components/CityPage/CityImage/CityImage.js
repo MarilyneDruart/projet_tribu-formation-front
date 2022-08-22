1387c1335ff32212bf434925e9c6b0d8951779ec
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-22 23:23:39 +0200

    creating CityImage component and scss file

---
+++ tribu/src/Components/CityPage/CityImage/CityImage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../../../styles/styles.scss';
+
+function CityImage() {
+  return (
+    <div className="CityImage">
+      <div className="CityImage_content">
+        <h2 className="CityImage_title">Nom de la ville</h2>
+      </div>
+    </div>
+  );
+}
+
+export default CityImage;
