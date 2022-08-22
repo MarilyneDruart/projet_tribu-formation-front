4421c29092bb42710807e50bb89c18611899709d
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-22 23:51:32 +0200

    first test for style tag inside a div in JSX

--- tribu/src/Components/CityPage/CityImage/CityImage.js
+++ tribu/src/Components/CityPage/CityImage/CityImage.js
@@ -3,8 +3,14 @@ import React from 'react';
 import '../../../styles/styles.scss';
 
 function CityImage() {
+  const image = `
+  div {
+    background-image: url('../assets/images/main-image2.jpg');
+  }
+  `;
   return (
     <div className="CityImage">
+      <style>{image}</style>
       <div className="CityImage_content">
         <h2 className="CityImage_title">Nom de la ville</h2>
       </div>
