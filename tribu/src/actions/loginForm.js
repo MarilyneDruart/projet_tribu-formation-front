66c25f48ee07855e63126322f8563bb73ea05a03
LaurentArcos <laurent.arcos@gmail.com>
2022-08-23 11:45:58 +0200

    login action file created

---
+++ tribu/src/actions/loginForm.js
@@ -0,0 +1,9 @@
+export const TOGGLE_LOGIN_FORM = 'TOGGLE_LOGIN_FORM';
+export const toggleLoginForm = () => ({
+  type: TOGGLE_LOGIN_FORM,
+});
+
+export const CLOSE_LOGIN_FORM = 'CLOSE_LOGIN_FORM';
+export const closeLoginForm = () => ({
+  type: CLOSE_LOGIN_FORM,
+});
