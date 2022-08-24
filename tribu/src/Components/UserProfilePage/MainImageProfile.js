98cfd21d88afcbdd84c48f670d54a3f354080ed2
LaurentArcos <laurent.arcos@gmail.com>
2022-08-24 16:47:48 +0200

    Profile Main image and avatar place are settled

---
+++ tribu/src/Components/UserProfilePage/MainImageProfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../styles/styles.scss';
+
+function MainImageProfile() {
+  return (
+    <div className="MainImageProfile">
+      <div className="MainImageProfile_avatar" />
+    </div>
+  );
+}
+
+export default MainImageProfile;
--- tribu/src/Components/UserProfilePage/UserProfilePage.js
+++ tribu/src/Components/UserProfilePage/UserProfilePage.js
@@ -1,12 +1,19 @@
+/* eslint-disable max-len */
 import React from 'react';
+
+import MainImageProfile from './MainImageProfile';
 
 // Imports locaux
 import '../../styles/styles.scss';
-import MainImage from '../MainImage/MainImage';
 
 function UserProfilePage() {
   return (
-    <MainImage />
+    <div className="Home">
+      <MainImageProfile />
+      <h2 className="Home_name">Prénom NOM</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab placeat libero nemo iure nobis, voluptate assumenda doloribus pariatur nesciunt! Consequuntur fugiat culpa dolore quod maiores repellendus facilis nam unde earum.</p>
+      <h3>Mes intérêts</h3>
+    </div>
   );
 }
 
