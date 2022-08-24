c3c8e22f42c0b7c9889cf43f07a99310593885e0
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-24 17:58:00 +0200

    test to get an interest with its id

--- tribu/src/Components/Interest/Interest.js
+++ tribu/src/Components/Interest/Interest.js
@@ -1,19 +1,31 @@
 import { React } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import '../../styles/styles.scss';
 
 function Interest() {
-  const { slug } = useParams();
+  const { id } = useParams();
+  console.log(id);
 
-  const findInterest = (interestsList, searchedSlug) => {
-    const interest = interestsList.find((testedInterest) => testedInterest.slug === searchedSlug);
+  const findInterest = (interestsList, searchedId) => {
+    const interest = interestsList.find((testedInterest) => testedInterest.id === searchedId);
+    console.log(interestsList);
+    console.log(interest);
+    console.log(searchedId);
+
+    if (id !== searchedId) {
+      <Navigate to="/NotFound" replace />;
+    }
     return interest;
   };
 
-  const currentInterest = useSelector((state) => findInterest(state.interests.list, slug));
+  const currentInterest = useSelector((state) => findInterest(state.interests.list, id));
   console.log(currentInterest);
+
+  if (!currentInterest) {
+    return <Navigate to="/PageIntrouvable" replace />;
+  }
 
   return (
     <div className="Interest">
