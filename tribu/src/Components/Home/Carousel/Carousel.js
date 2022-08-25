096377e14e4982336f1a4a40d512b8719fe29b77
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-25 22:20:37 +0200

    carousel base structure created

--- tribu/src/Components/Home/Carousel/Carousel.js
+++ tribu/src/Components/Home/Carousel/Carousel.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
 import '../../../styles/styles.scss';
+import img1 from '../../../assets/images/carousel/1.jpg';
+import img2 from '../../../assets/images/carousel/2.jpg';
+import img3 from '../../../assets/images/carousel/3.jpg';
+import img4 from '../../../assets/images/carousel/4.jpg';
+import img5 from '../../../assets/images/carousel/5.jpg';
+import img6 from '../../../assets/images/carousel/6.jpg';
 
 function Carousel() {
+  const slides = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+  ];
+
   return (
     <div className="container">
       <div className="Carousel">
         <div className="Carousel-inner">
-          <div className="Carousel-item">
-            <img src="" alt="" />
-          </div>
+          {slides.map((slide) => (
+            <div className="Carousel-item" key={slide}>
+              <img src={slide} alt="" />
+            </div>
+          ))}
         </div>
       </div>
     </div>
