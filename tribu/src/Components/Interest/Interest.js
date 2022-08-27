708620ff1071eff4440f2d5b53c85d59df7d1fe7
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-27 21:58:15 +0200

    displaying event date when it has one

--- tribu/src/Components/Interest/Interest.js
+++ tribu/src/Components/Interest/Interest.js
@@ -1,11 +1,13 @@
 import { React } from 'react';
 import { useParams, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import calendar from '../../assets/images/timetable.png';
 
 import '../../styles/styles.scss';
 
 function Interest() {
   const { id } = useParams();
+
   const interestsList = useSelector((state) => state.interests.list);
 
   const currentInterest = interestsList.find((interest) => interest.id === Number(id));
@@ -22,20 +24,25 @@ function Interest() {
       <div className="Interest_description">
         <div className="Interest_description_category">
           {currentInterest.category.map((category) => (
-            <span className="Interest_description_category_item">{category.name}</span>
+            <span className="Interest_description_category_item" key={category.id}>{category.name}</span>
           ))}
+        </div>
+
+        <div className="Interest_description_date">
+          {currentInterest.date && <img className="Interest_description_date_icon" src={calendar} alt="icone calendrier" />}
+          {currentInterest.date}
         </div>
 
         <h1 className="Interest_description_title">{currentInterest.title}</h1>
         <p className="Interest_description_content">{currentInterest.content}</p>
         <p className="Interest_description_author">
-          créé par
+          publié par
           {' '}
           {currentInterest.user.firstname}
           {' '}
           {currentInterest.user.lastname}
         </p>
-        <p className="Interest_description_date">
+        <p className="Interest_description_publishingdate">
           le
           {' '}
           {currentInterest.createdAt}
