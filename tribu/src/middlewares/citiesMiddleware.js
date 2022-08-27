2f4265369a7e2742b80262709101630120a1f09f
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-27 16:31:31 +0200

    API URL changed to local ones

--- tribu/src/middlewares/citiesMiddleware.js
+++ tribu/src/middlewares/citiesMiddleware.js
@@ -8,7 +8,7 @@ const citiesMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case FETCH_CITIES:
       axios
-        .get('https://pierre-henri-kocan-server.eddi.cloud/projet-reseau-social-back/public/api/cities')
+        .get('http://lola-costa.vpnuser.lan:8000/api/cities')
         .then((response) => {
           store.dispatch(setCitiesList(response.data));
         })
