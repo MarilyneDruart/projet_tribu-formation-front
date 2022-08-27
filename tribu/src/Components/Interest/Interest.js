c712608425edb4c0033feae6ee0334376d6e985d
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-27 22:34:06 +0200

    comments in code

--- tribu/src/Components/Interest/Interest.js
+++ tribu/src/Components/Interest/Interest.js
@@ -7,12 +7,23 @@ import '../../styles/styles.scss';
 
 function Interest() {
   const { id } = useParams();
-
   const interestsList = useSelector((state) => state.interests.list);
-
   const currentInterest = interestsList.find((interest) => interest.id === Number(id));
 
   console.log(currentInterest);
+
+  // Converting YYYY-MM-DD into french format
+  const dateInFrench = (dateToChange) => {
+    const day = (dateToChange.slice(8, 10));
+    let month = (dateToChange.slice(5, 7));
+    const year = (dateToChange.slice(0, 4));
+
+    // Turning month number into name
+    // eslint-disable-next-line
+    month === '01' ? month = 'janvier' : month === '02' ? month = 'février' : month === '03' ? month = 'mars' : month === '04' ? month = 'avril' : month === '05' ? month = 'mai' : month === '06' ? month = 'juin' : month === '07' ? month = 'juillet' : month === '08' ? month = 'août' : month === '09' ? month = 'septembre' : month === '10' ? month = 'octobre' : month === '11' ? month = 'novembre' : month === '12' ? month = 'décembre' : ''
+
+    return `le ${day} ${month} ${year}`;
+  };
 
   if (!currentInterest) {
     return <Navigate to="/PageIntrouvable" replace />;
@@ -28,10 +39,12 @@ function Interest() {
           ))}
         </div>
 
-        <div className="Interest_description_date">
-          {currentInterest.date && <img className="Interest_description_date_icon" src={calendar} alt="icone calendrier" />}
-          {currentInterest.date}
-        </div>
+        {currentInterest.date && (
+          <div className="Interest_description_date">
+            <img className="Interest_description_date_icon" src={calendar} alt="icone calendrier" />
+            {dateInFrench(currentInterest.date)}
+          </div>
+        )}
 
         <h1 className="Interest_description_title">{currentInterest.title}</h1>
         <p className="Interest_description_content">{currentInterest.content}</p>
@@ -43,9 +56,7 @@ function Interest() {
           {currentInterest.user.lastname}
         </p>
         <p className="Interest_description_publishingdate">
-          le
-          {' '}
-          {currentInterest.createdAt}
+          {dateInFrench(currentInterest.createdAt)}
         </p>
       </div>
 
