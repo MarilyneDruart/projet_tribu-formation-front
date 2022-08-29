b985ef6e765a5bd24c323dcfda89ed080fdc97c5
LolaCosta-DeVinci <lolacostapro@gmail.com>
2022-08-29 10:49:07 +0200

    categoriesMiddleware added to the store

--- tribu/src/store/store.js
+++ tribu/src/store/store.js
@@ -7,12 +7,19 @@ import debugMiddleware from '../middlewares/debugMiddleware';
 import citiesMiddleware from '../middlewares/citiesMiddleware';
 import interestsMiddleware from '../middlewares/interestsMiddleware';
 import usersMiddleware from '../middlewares/usersMiddleware';
+import categoriesMiddleware from '../middlewares/categoriesMiddleware';
 
 // eslint-disable-next-line no-unused-vars
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const myMiddleWares = composeEnhancers(
-  applyMiddleware(debugMiddleware, citiesMiddleware, interestsMiddleware, usersMiddleware),
+  applyMiddleware(
+    debugMiddleware,
+    citiesMiddleware,
+    interestsMiddleware,
+    categoriesMiddleware,
+    usersMiddleware,
+  ),
 );
 
 const store = createStore(
